edfa83fb6a04e3fcb034c33ba1e61b0b62d691a3
Nash0567 <ellisnash05@gmail.com>
2025-08-22 21:56:52 -0500

    Module 7 completed baseline

---
+++ app_admin/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, importProvidersFrom, Provider } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+import { JwtInterceptor } from './utils/jwt.interceptor';
+export const authInterceptProvider: Provider =
+{
+  provide: HTTP_INTERCEPTORS,
+  useClass: JwtInterceptor, multi: true
+};
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    authInterceptProvider
+  ]
+};
